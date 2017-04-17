c66098a72dfeb83a3100998b1bdef12219ae9ce0
Farita Tasnim <farita@mit.edu>
2017-04-17 19:43:05 -0400

    breadboard add

--- js/grid.js
+++ js/grid.js
@@ -5,37 +5,22 @@ $(document).ready(function () {
     "use strict";
     var img1 = "<IMG SRC='/images/dg.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
 
-   for (var d=0; d < 2; d++){
-       $("body").append("\u00A0");
-        for (var c=0; c < 10; c++){
-            $("body").append("\u00A0"+"\u00A0"+"\u00A0");
-            for (var b=0; b < 5; b++){
-                    $("body").append("\u00A0" + img1);
-                }
-        }
-       $("body").append("<br>");
-   }
+    end();
     
     $("body").append("<br>");
     
-    for (var j=0; j < 5; j++){
-        for (var i=0; i < 64; i++){
-            $("body").append("\u00A0" + img1);
-        }
-        $("body").append("<br>");
-    }
+    mid();
     
     $("body").append("<br>");
     
-    for (var j=0; j < 5; j++){
-        for (var i=0; i < 64; i++){
-            $("body").append("\u00A0" + img1);
-        }
-        $("body").append("<br>");
-    }
+    mid();
     
     $("body").append("<br>");
     
+    end();
+});
+
+function end () {
     for (var d=0; d < 2; d++){
        $("body").append("\u00A0");
         for (var c=0; c < 10; c++){
@@ -46,4 +31,14 @@ $(document).ready(function () {
         }
        $("body").append("<br>");
    }
-});
+}
+
+function mid () {
+    for (var j=0; j < 5; j++){
+        for (var i=0; i < 64; i++){
+            $("body").append("\u00A0" + img1);
+        }
+        $("body").append("<br>");
+    }
+}
+}
