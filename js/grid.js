903641c6e368bcc0e125d2adb4f10be1deb0cf5b
Farita Tasnim <farita@mit.edu>
2017-04-17 19:31:46 -0400

    breadboard add

--- js/grid.js
+++ js/grid.js
@@ -4,10 +4,11 @@
 $(document).ready(function () {
     "use strict";
     var img1 = "<IMG SRC='/images/dg.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
-    
+
+   $("body").append("\u00A0"+"\u00A0");
    for (var d=0; d < 2; d++){
         for (var c=0; c < 10; c++){
-            $("body").append("\u00A0"+"\u00A0"+"\u00A0"+"\u00A0"+"\u00A0");
+            $("body").append("\u00A0"+"\u00A0"+"\u00A0");
             for (var b=0; b < 5; b++){
                     $("body").append("\u00A0" + img1);
                 }
