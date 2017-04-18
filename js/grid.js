1aee89d06b49de2fc2b293050ed73ccedfcc815f
Farita Tasnim <farita@mit.edu>
2017-04-18 16:13:18 -0400

    breadboard add

--- js/grid.js
+++ js/grid.js
@@ -2,6 +2,7 @@
 /*global $, jQuery, alert*/
 
 var tag = 0;
+var draw = SVG('container')
     
 $(document).ready(function () {
     "use strict";
@@ -23,24 +24,26 @@ $(document).ready(function () {
     //$("div").append("<p>"+tag+"</p>");
     
     //use adjacency lists
+    //replace images with svg's
 });
 
 
 function end () {
     var img1 = "<IMG SRC='/images/dg.png'"+"ID='"+tag+"' STYLE='WIDTH:7px; HEIGHT:7px'>";
+    var hole = draw.circle(7);
     var img2 = "<IMG SRC='/images/plus.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
     var img3 = "<IMG SRC='/images/minus.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
     for (var d=0; d < 2; d++){
         if (d==0) $("div").append(img2);
         else $("div").append(img3);
         for (var b=0; b < 5; b++){
-            $("div").append("\u00A0" + img1);
+            $("div").append("\u00A0" + hole);
             tag++;
         }
         for (var c=1; c < 10; c++){
             $("div").append("\u00A0"+"\u00A0"+"\u00A0");
             for (var b=0; b < 5; b++){
-                $("div").append("\u00A0" + img1);
+                $("div").append("\u00A0" + hole);
                 tag++;
             }
         }
@@ -52,7 +55,7 @@ function mid () {
     var img1 = "<IMG SRC='/images/dg.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
     for (var j=0; j < 5; j++){
         for (var i=0; i < 64; i++){
-            $("div").append("\u00A0" + img1);
+            $("div").append("\u00A0" + hole);
             tag++;
         }
         $("div").append("<br>");
