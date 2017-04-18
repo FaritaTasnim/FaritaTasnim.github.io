84bf26da9b73f74060a1b0d5c54c5a58ac7f309b
Farita Tasnim <farita@mit.edu>
2017-04-17 23:13:25 -0400

    breadboard add

--- js/grid.js
+++ js/grid.js
@@ -24,11 +24,24 @@ $(document).ready(function () {
     
     var arr = [];
     for(var x = 0; x < 840; x++){
-        arr[x] = [];    
+        arr[x] = [];
         for(var y = 0; y < 840; y++){ 
-            arr[x][y] = 0;    
+            if (0<=x<=49 && 0<=y<=49) arr[x][y] = 1;
+            else if (50<=x<=99 && 50<=y<=99) arr[x][y] = 1;
+            else if (740<=x<=789 && 740<=y<=789) arr[x][y] = 1;
+            else if (790<=x<=839 && 790<=y<=839) arr[x][y] = 1;
+            else if (100<=x<=163 && (y==x+64 || y==x+2*64 || y==x+3*64 || y==x+3*64 || y==x+5*64)) arr[x][y] = 1;
+            else if (420<=x<=483 && (y==x+64 || y==x+2*64 || y==x+3*64 || y==x+3*64 || y==x+5*64)) arr[x][y] = 1;
+            else if (100<=y<=163 && (x==y+64 || x==y+2*64 || x==y+3*64 || x==y+3*64 || x==y+5*64)) arr[x][y] = 1;
+            else if (420<=y<=483 && (x==y+64 || x==y+2*64 || x==y+3*64 || x==y+3*64 || x==y+5*64)) arr[x][y] = 1;
+            else arr[x][y] = 0;    
         }    
     }
+    
+    $("div").append("<p>"+arr[100][292]+"</p>");
+    $("div").append("<p>"+arr[292][100]+"</p>");
+    $("div").append("<p>"+arr[100][101]+"</p>");
+    $("div").append("<p>"+arr[101][100]+"</p>");
 });
 
 
