6bca0b6d9733754b3806d7ab74fd08f01adf92c7
Farita Tasnim <farita@mit.edu>
2017-04-17 20:21:51 -0400

    breadboard add

--- js/grid.js
+++ js/grid.js
@@ -23,7 +23,6 @@ $(document).ready(function () {
 function end () {
     var img1 = "<IMG SRC='/images/dg.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
     for (var d=0; d < 2; d++){
-       $("div").append("\u00A0");
         for (var c=0; c < 10; c++){
             $("div").append("\u00A0"+"\u00A0"+"\u00A0");
             for (var b=0; b < 5; b++){
