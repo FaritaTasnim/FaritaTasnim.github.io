2a42f7458e86935c66d087f817de8f59bd798acb
Farita Tasnim <farita@mit.edu>
2017-04-17 21:34:36 -0400

    breadboard add

--- js/grid.js
+++ js/grid.js
@@ -1,6 +1,8 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
+var tag = 0;
+    
 $(document).ready(function () {
     "use strict";
 
@@ -21,7 +23,7 @@ $(document).ready(function () {
 
 
 function end () {
-    var img1 = "<IMG SRC='/images/dg.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
+    var img1 = "<IMG SRC='/images/dg.png'"+"ID='"+tag+"' STYLE='WIDTH:7px; HEIGHT:7px'>";
     var img2 = "<IMG SRC='/images/plus.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
     var img3 = "<IMG SRC='/images/minus.png' STYLE='WIDTH:7px; HEIGHT:7px'>";
     for (var d=0; d < 2; d++){
@@ -29,11 +31,13 @@ function end () {
         else $("div").append(img3);
         for (var b=0; b < 5; b++){
             $("div").append("\u00A0" + img1);
+            tag++;
         }
         for (var c=1; c < 10; c++){
             $("div").append("\u00A0"+"\u00A0"+"\u00A0");
             for (var b=0; b < 5; b++){
                 $("div").append("\u00A0" + img1);
+                tag++;
             }
         }
        $("div").append("<br>");
@@ -45,6 +49,7 @@ function mid () {
     for (var j=0; j < 5; j++){
         for (var i=0; i < 64; i++){
             $("div").append("\u00A0" + img1);
+            tag++;
         }
         $("div").append("<br>");
     }
