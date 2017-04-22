a4583e5bcb3af57fbf952f21996f42b447beae35
Farita Tasnim <farita@mit.edu>
2017-04-22 14:44:19 -0400

    breadboard add

---
+++ js/bb.js
@@ -0,0 +1,56 @@
+var tag = 0;
+$(document).ready(function() {
+    var s = Snap(1000,2000);
+    
+
+    end(s, 100, 50);
+    
+    mid(s, 74, 102);
+    
+    mid(s, 74, 193);
+
+    end(s, 100, 284);
+    
+    $.each(s.selectAll("circle").items, function() {
+
+        this.mouseover(function() {
+            this.animate({
+                fill: "#ff0000"
+            }, 100);
+        }).mouseout(function() {
+            this.stop();
+            this.animate({
+                fill: "#000000"
+            }, 500);
+        }).click(function() {
+            
+        });
+    });
+    
+});
+
+function end(s, x, y){
+    for (var c=0; c < 2; c++) {
+        if (c==0) s.text(x-17, y+5, "+");
+        else s.text(x-15, y+4, "-");
+        for (var b=0; b < 10; b++){
+            for (var a=0; a < 5; a++){
+                s.circle(x+a*13, y, 3).attr({ id: tag});
+                tag++;
+            }
+            x = x + 78;
+        }
+        x = 100;
+        y = y + 13;
+    }
+}
+
+function mid(s, x, y){
+    for (var b=0; b < 63; b++){
+        for (var a=0; a < 5; a++){
+            s.circle(x, y+a*13, 3).attr({ id: tag});
+            tag++;
+        }
+        x = x + 13;
+    }
+}
