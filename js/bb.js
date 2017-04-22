555c1da643da829c20d33337c274cbf2fb801fc5
Farita Tasnim <farita@mit.edu>
2017-04-22 16:30:12 -0400

    breadboard add

--- js/bb.js
+++ js/bb.js
@@ -2,7 +2,6 @@ var tag = 0;
 $(document).ready(function() {
     var s = Snap(1000,2000);
     
-
     end(s, 100, 50);
     
     mid(s, 74, 102);
@@ -17,16 +16,56 @@ $(document).ready(function() {
             this.attr({
                 fill: "#ff0000"
             });
+            var sel = this.attr("id");
+            show_conn(s, sel, 0, 50);
+            show_conn(s, sel, 50, 100);
+            show_conn(s, sel, 730, 780);
+            show_conn(s, sel, 780, 830);
+            for (i=0; i < 126; i++){
+                show_conn(s, sel, 100+i*5, 105+i*5);
+            }
         }).mouseout(function() {
             this.attr({
                 fill: "#000000"
             });
+            var sel = this.attr("id");
+            hide_conn(s, sel, 0, 50);
+            hide_conn(s, sel, 50, 100);
+            hide_conn(s, sel, 730, 780);
+            hide_conn(s, sel, 780, 830);
+            for (i=0; i < 126; i++){
+                hide_conn(s, sel, 100+i*5, 105+i*5);
+            }
         }).click(function() {
             
         });
     });
     
 });
+
+function show_conn(s, sel, lo, hi){
+    if (lo <= sel && sel < hi){
+        $.each(s.selectAll("circle").items, function() {
+            if (lo <= this.attr("id") && this.attr("id") < hi && this.attr("id") != sel){
+                this.attr({
+                    fill: "#4dbd4d"
+                });
+            }
+        });
+    }
+}
+
+function hide_conn(s, sel, lo, hi){
+    if (lo <= sel && sel < hi){
+        $.each(s.selectAll("circle").items, function() {
+            if (lo <= this.attr("id") && this.attr("id") < hi){
+                this.attr({
+                    fill: "#000000"
+                });
+            }
+        });
+    }
+}
 
 function end(s, x, y){
     for (var c=0; c < 2; c++) {
