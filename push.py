2fa650182008db7db179e0696a507016b993e859
Farita Tasnim <farita@mit.edu>
2017-12-04 18:38:58 -0500

    bitch

--- push.py
+++ push.py
@@ -2,7 +2,7 @@ import os, sys
 
 os.system('git pull origin master')
 os.system('git add --all')
-os.system('git commit -m "some message"')
+os.system('git commit -m "' + sys.argv[1] + '"')
 os.system('git push -u origin master')
 
 print(sys.argv[1])
