67450a5751731ef2f0788e41077f7de9df51ff90
Farita Tasnim <farita@mit.edu>
2017-12-04 18:37:18 -0500

    some message

--- push.py
+++ push.py
@@ -1,6 +1,8 @@
-import os
+import os, sys
 
 os.system('git pull origin master')
 os.system('git add --all')
 os.system('git commit -m "some message"')
 os.system('git push -u origin master')
+
+print(sys.argv[1])
