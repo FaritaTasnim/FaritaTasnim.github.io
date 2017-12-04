c1e892fad6373103feae29c7ccdf9d249b718421
Farita Tasnim <farita@mit.edu>
2017-12-04 18:40:59 -0500

    I like to move it move it

--- push.py
+++ push.py
@@ -2,7 +2,7 @@ import os, sys
 
 os.system('git pull origin master')
 os.system('git add --all')
-os.system('git commit -m "' + sys.argv[1] + '"')
+os.system('git commit -m "' + " ".join(sys.argv[1:]) + '"')
 os.system('git push -u origin master')
 
 print(sys.argv[1])
